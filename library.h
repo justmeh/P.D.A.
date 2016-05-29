e4ad79a299d07a9da6ce07319031fd60f68aba43
Aleen1 <alin.calina@yahoo.com>
2016-05-29 17:29:54 +0300

    added dynamic algorithms

--- library.h
+++ library.h
@@ -1,51 +1,58 @@
 #ifndef library
 #define library
 
+    int adj_mat[100][100];
+
     typedef struct Nod{
         int data;
         struct Nod * next;
     }Nod;
 
-    //Push functions for single-linked lists
+    /**Push functions for single-linked lists**/
     void print_list(Nod *head);
     void push_first(Nod *head, int value);
     void push_last(Nod *head, int value);
     void push_poz(Nod *head, int poz, int value);
 
-    //Pop functions for single-linked lists
+    /**Pop functions for single-linked lists**/
     int pop_first(Nod * head);
     int pop_last(Nod * head);
     int pop_poz(Nod *head, int poz);
 
-    //Read/Print functions
+    /**Read/Print functions**/
     void printArray(int arr[], int n);
     void readArray(int arr[], int n);
     void readMat(int a[100][100], int n);
 
-    //Heap sort funtions
+    /**Heap sort funtions**/
     void heapify(int arr[], int n, int i);
     void heapSort(int arr[], int n);
 
-    //Merge sort functions
+    /**Merge sort functions**/
     void mergeArray(int arr[], int l, int m, int r);
     void mergeSort(int arr[], int l, int r);
 
-    //Quick sort functions
+    /**Quick sort functions**/
     void swapp(int* a, int* b);
     int partitionn(int arr[], int l, int h);
     void quickSort(int arr[], int l, int h);
 
-    //Radix sort funtions
+    /**Radix sort funtions**/
     int getMax(int arr[], int n);
     void countSort(int arr[], int n, int exp);
     void radixsort(int arr[], int n);
 
-    //DFS function
-    void dfs(int root, int a[100][100], int visited[], int n);
+    /**Traversing graphs/trees functions**/
+    void dfs_mat(int root, int n);
+    void bfs_mat(int root, int n);
 
-    //Backtracking standard functions
-    int printSolution();
+    /**Backtracking standard functions**/
+    void printSolution();
     int valid();
     void backtracking();
+
+    /**Dynamic Programming algorithms**/
+    int count_coin( int S[], int m, int n );    // need to read m(nr of coins) and n(the sum)
+    int knapsack(int n, int g);                 // need to read n(nr of obj) and g(max value)
 
 #endif
