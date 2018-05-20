4c2100eeac02790780cddf186128019c9474269a
Robert Stoenica <eternal.rivals@hotmail.com>
2018-05-20 23:15:34 +0300

    Update RoyFloyd.cu

--- Lab7/RoyFloyd.cu
+++ Lab7/RoyFloyd.cu
@@ -24,13 +24,13 @@ int main()
     // Allocate input vectors h_A and h_B in host memory
 
     // Initialize input vectors
-	int h_graph[N][N] = {
-		0,3,9,8,3,
-		5,0,1,4,2,
-		6,6,0,4,5,
-		2,9,2,0,7,
-		7,9,3,2,0,
-	};
+    int h_graph[N][N] = {
+	0,3,9,8,3,
+	5,0,1,4,2,
+	6,6,0,4,5,
+	2,9,2,0,7,
+       	7,9,3,2,0,
+   };
 	
     // Allocate vectors in device memory
     int* d_graph;
@@ -41,13 +41,12 @@ int main()
 
     // Invoke kernel
     dim3 threadsPerBlock(N, N);
-	int numBlocks= 1;
+    int numBlocks= 1;
 
-	for(int k = 0; k < N; k++)
-	{
-		VecAdd<<<numBlocks, threadsPerBlock>>>(d_graph, k);
-
-	}
+    for(int k = 0; k < N; k++)
+    {
+	    matrixFloyd<<<numBlocks, threadsPerBlock>>>(d_graph, k);
+    }
 		
     // Copy result from device memory to host memory
     // h_C contains the result in host memory
